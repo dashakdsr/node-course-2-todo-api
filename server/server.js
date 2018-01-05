e9a23eac1b2e6103c751aa78f26416b5081ad711
Kyrychenko <darya.kyrychenko@ekreative.com>
2018-01-05 20:05:47 +0200

    setup mongoose && add POST /todos

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {User} = require('./models/user')
+const {Todo} = require('./models/todo')
+
+let app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  console.log(req.body)
+  let todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then((doc) => {
+    res.send(doc)
+  }, (err) => {
+    console.log('Unable to save ', err)
+    res.status(400).send(err)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Started on port 3000')
+})
