f2c35436a01347eaa2d85296875689387ddfe49a
Kyrychenko <darya.kyrychenko@ekreative.com>
2018-01-06 16:53:49 +0200

    add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -50,6 +50,22 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id not found')
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('Id not found')
+    }
+    res.send({todo})
+  }, (err) => {
+    res.status(400).send(err)
+  })
+})
+
 app.listen(port, () => {
   console.log('Started on port ', port)
 })
