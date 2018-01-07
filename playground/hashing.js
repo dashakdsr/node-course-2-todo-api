da8b843b11f6e64aac7e58de231bb91ff17ed01a
Kyrychenko <darya.kyrychenko@ekreative.com>
2018-01-07 18:17:48 +0200

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,27 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-let data = {
-  id: 10
-}
+let password = '123abc'
 
-let token = jwt.signin(data, '123abc')
-let decoded = jwt.verify(token, '123abc')
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      console.log(hash)
+    })
+})
+
+let hashedPassword = '$2a$10$XlQ06DRjpi01dYnkuvjh6uo3XweX8RcuTxWLmAu84mLVnqK5AUvfO'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
+
+// let data = {
+//   id: 10
+// }
+//
+// let token = jwt.signin(data, '123abc')
+// let decoded = jwt.verify(token, '123abc')
 
 // let message = 'I am user number 3'
 // let hash = SHA256(message).toString()
